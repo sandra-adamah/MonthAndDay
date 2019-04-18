2002bf0f29d6b35e9d8b138cb5c71150e6d6a9bf
Sandra <sandra.adamah-tassah.2021@studen.lhschools.org>
2019-04-18 13:22:39 -0400

    Inserted nursery

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,6 +3,29 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	88888888
+
+        System.out.println("Would you like to know more about yourself?");
+        System.out.println("Here is a nursery rhyme that can tell you about your character based on the day you were born");
+        System.out.println("                                                       ");
+
+        System.out.println("Monday's child is fair of face,\n" +
+                "Tuesday's child is full of grace,\n" +
+                "Wednesday's child is full of woe,\n" +
+                "Thursday's child has far to go.\n" +
+                "Friday's child is loving and giving,\n" +
+                "Saturday's child works hard for a living,\n" +
+                "But the child born on the Sabbath Day,\n" +
+                "Is fair and wise and good in every way.");
+
+
+
+
+
+
+
+
+
     }
+
+
 }
